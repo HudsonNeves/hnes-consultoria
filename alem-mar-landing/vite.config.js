fecfb5d474c01715564e1801444d4a30bc95e8d2
SILVA, H. N. <hudsonnsilva@gmail.com>
2026-04-29 11:26:56 -0300

    Delete alem-mar-landing directory

--- alem-mar-landing/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [],
-  base: '/alem-mar-landing/', // Ensure correct base path for GitHub Pages
-});
